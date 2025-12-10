e8920a8ab032846fac0b5fb7ef5fbafa7b3869b2
Matthias Spiller <matthias.spiller@fari-software.de>
2025-12-10 12:16:36 +0100

    Day 10

---
+++ 10/10_1.py
@@ -0,0 +1,75 @@
+from ortools.sat.python import cp_model
+
+problems = []
+
+with open("input_10.txt") as f:
+    for line in f.readlines():
+        b = line[: line.index("]")]
+
+        target = 0
+        for i, c in enumerate(b[1:]):
+            if c == "#":
+                target += 2 ** (i)
+
+        num_switches = len(b[1:])
+
+        sw = line[line.index("] ") + 2 : line.index("{")].strip().split(" ")
+        switches = [[int(i) for i in s[1:-1].split(",")] for s in sw]
+
+        problems.append((b[1:], num_switches, switches))
+
+
+class VarArraySolutionPrinter(cp_model.CpSolverSolutionCallback):
+    """Print intermediate solutions."""
+
+    def __init__(self, variables: list[cp_model.IntVar]):
+        cp_model.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+
+    def on_solution_callback(self) -> None:
+        for v in self.__variables:
+            print(f"{v}={self.value(v)}", end=" ")
+        print()
+
+
+def solve_problem(p):
+    b, n_s, sw = p
+
+    model = cp_model.CpModel()
+
+    num_presses = []
+
+    for i, l in enumerate(sw):
+        presses = model.new_int_var(0, 1000, f"n{i}")
+        num_presses.append(presses)
+
+    for j, b in enumerate(b):
+        t = [num_presses[i] for i, s in enumerate(sw) if j in s]
+        total = model.new_int_var(0, 1000 * len(t), f"sum_{j}")
+        model.add(total == sum(t))
+        eq = model.add_modulo_equality(
+            1 if b == "#" else 0,
+            total,
+            2,
+        )
+
+    model.minimize(sum(num_presses))
+
+    print(model.Validate())
+
+    solver = cp_model.CpSolver()
+    solution_printer = VarArraySolutionPrinter(num_presses)
+    status = solver.solve(model, solution_printer)
+
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print(f"Maximum of objective function: {solver.objective_value}\n")
+        return int(solver.objective_value)
+    else:
+        print("No solution found.")
+
+
+ans = 0
+for p in problems:
+    ans += solve_problem(p)
+
+print(ans)
